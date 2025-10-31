0a06b728773562352c368ba1147cda68ff9ab912
katzeNilpferd <evgeny.ockatiev@gmail.com>
2025-10-31 09:06:56 +0300

    An annotation has been added for the "/tvip_provision.xml" endpoint.

--- src/presentation/api/endpoints/provision.py
+++ src/presentation/api/endpoints/provision.py
@@ -1,24 +1,27 @@
-from fastapi import APIRouter, Request, Response, HTTPException, Depends
+from fastapi import APIRouter, Response, Depends, Header
+from typing import Annotated
 
 from application.use_cases.tvip_provision.handle_provision_request import HandleProvisionRequestUseCase
 from infrastructure.di.injection import get_handle_provision_use_case
 
 
-router = APIRouter()
+router = APIRouter(prefix='/prov', tags=['Provision.xml'])
 
 
-@router.get('/prov/tvip_provision.xml')
+@router.get('/tvip_provision.xml', summary='Get XML config')
 async def tvip_provision_endpoint(
-    request: Request,
+    mac_address: Annotated[str, Header(alias='Mac-Address')],
     use_case: HandleProvisionRequestUseCase = Depends(get_handle_provision_use_case)
-):  
-    mac_address = request.headers.get('Mac-Address')
+):      
+    """
+    Generate XML configuration for TVIP device.
     
-    if not mac_address:
-        raise HTTPException(
-            status_code=400,
-            detail='Mac-Address header is required'
-        )
-    
+    Args:
+        mac_address: Device MAC address from request headers.
+        use_case: Business logic handler (injected automatically).
+
+    Returns:
+        Response: Device-specific settings.
+    """
     xml_content = await use_case.execute(mac_address)
     return Response(content=xml_content, media_type="application/xml")
