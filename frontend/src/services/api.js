883346a2c0f1dd1aa728d1a802e156fc33d37927
katzeNilpferd <evgeny.ockatiev@gmail.com>
2025-11-13 11:11:23 +0300

    New APIs have been added, and unnecessary ones have been removed.

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -20,18 +20,23 @@ export const getDeviceConfig = async (macAddress) => {
   return response.data
 }
 
-export const updateDeviceConfig = async (macAddress, updates) => {
-  const response = await api.put(`/devices/${macAddress}/config`, updates)
+export const replaceDeviceConfig = async (macAddress, newConfig) => {
+  const response = await api.put(`/devices/${macAddress}/config/replace`, newConfig)
   return response.data
 }
 
 export const resetDeviceConfig = async (macAddress) => {
-  const response = await api.post(`/devices/${macAddress}/reset-config`)
+  const response = await api.post(`/devices/${macAddress}/config/reset`)
   return response.data
 }
 
-export const getConfigs = async () => {
-  const response = await api.get('/configs')
+export const getDefaultConfig = async () => {
+  const response = await api.get('/default/config')
+  return response.data
+}
+
+export const replaceDefaultConfig = async (newConfig) => {
+  const response = await api.put('/default/config/replace', newConfig)
   return response.data
 }
 
