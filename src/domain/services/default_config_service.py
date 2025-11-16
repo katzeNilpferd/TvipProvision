5529607b364bd6e42f33c31fca7a2c5c3816ab29
katzeNilpferd <evgeny.ockatiev@gmail.com>
2025-11-16 07:38:52 +0300

    Minor adjustments to validate_default_config.

--- src/domain/services/default_config_service.py
+++ src/domain/services/default_config_service.py
@@ -19,10 +19,5 @@ class DefaultConfigService:
     @staticmethod
     def validate_default_config(config_data: ConfigData) -> bool:
         """Валидация дефолтного конфига"""
-        required_params = [
-            'provision.@reload',
-            'provision.provision_server',
-            'provision.operator', 
-            'provision.syslog_host'
-        ]
-        return config_data.validate_structure(required_params)
+
+        return config_data.validate_structure()
