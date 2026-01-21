e56a1b86025c2104ce93f7c0b83d57d447bfbe83
katzeNilpferd <evgeny.ockatiev@gmail.com>
2026-01-21 15:09:03 +0300

    feat: Added handles to reset the password if you forgot the password.

--- src/presentation/api/endpoints/users_management.py
+++ src/presentation/api/endpoints/users_management.py
@@ -1,11 +1,20 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from typing import Any
 
+from domain.auth.entities.ticket import TicketType
 from application.use_cases.users_management.change_password import ChangePasswordUseCase
+from application.use_cases.users_management.password_recovery import PasswordRecoveryUseCase
+from application.use_cases.tickets.create_ticket import CreateTicketUseCase
 from presentation.api.dependencies.auth import get_current_active_user
-from presentation.api.models.users import ChangePasswordRequest
+from presentation.api.models.users import (
+    ChangePasswordRequest,
+    CreateTicketRequest,
+    PasswordRecoveryRequest
+)
 from infrastructure.di.injection import (
-    get_change_password_use_case
+    get_change_password_use_case,
+    get_create_ticket_use_case,
+    get_password_recovery_use_case
 )
 
 
@@ -28,6 +37,39 @@ async def change_user_password(
         return await use_case.execute(
             user_id=user_id,
             current_password=request.current_password,
+            new_password=request.new_password
+        )
+    except ValueError as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+
+
+@router.post('/api/users/forgot-password')
+async def forgot_user_password(
+    request: CreateTicketRequest,
+    use_case: CreateTicketUseCase = Depends(get_create_ticket_use_case)
+):
+    ticket_type = TicketType.FORGOT_PASSWORD
+    description = f'User {request.username} requested password reset' if not request.description else request.description
+
+    try:
+        return await use_case.execute(
+            username=request.username,
+            ticket_type=ticket_type,
+            description=description
+        )
+    except ValueError as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+
+
+@router.post('/api/users/password-recovery')
+async def password_recovery(
+    request: PasswordRecoveryRequest,
+    use_case: PasswordRecoveryUseCase = Depends(get_password_recovery_use_case)
+):
+    try:
+        return await use_case.execute(
+            username=request.username,
+            secret_key=request.secret_key,
             new_password=request.new_password
         )
     except ValueError as e:
--- src/presentation/api/models/users.py
+++ src/presentation/api/models/users.py
@@ -1,6 +1,16 @@
+from typing import Optional
 from pydantic import BaseModel
 
 
 class ChangePasswordRequest(BaseModel):
     current_password: str
+    new_password: str
+
+class CreateTicketRequest(BaseModel):
+    username: str
+    description: Optional[str] = None
+
+class PasswordRecoveryRequest(BaseModel):
+    username: str
+    secret_key: str
     new_password: str
