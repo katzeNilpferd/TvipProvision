eff611625472e4ca8b4552b3315515720b74c461
katzeNilpferd <evgeny.ockatiev@gmail.com>
2026-01-21 15:04:26 +0300

    refactor: Minor refactoring.

--- src/presentation/api/dependencies/auth.py
+++ src/presentation/api/dependencies/auth.py
@@ -27,7 +27,6 @@ async def get_current_user(
 async def get_current_active_user(
     current_user: dict[str, Any] = Depends(get_current_user)
 ):
-    print(current_user.get('username'))
     if not current_user.get('is_active'):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -40,7 +39,6 @@ async def get_current_active_user(
 async def get_current_admin_user(
     current_user: dict[str, Any] = Depends(get_current_user)
 ):
-    print(current_user.get('username'))
     if not current_user.get('is_admin'):
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
