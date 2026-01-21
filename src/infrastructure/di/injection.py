3693be2f7750311fc1d646df90dd682c957bb3f4
katzeNilpferd <evgeny.ockatiev@gmail.com>
2026-01-21 15:02:14 +0300

    refactor: Dependent injections has been updated.

--- src/infrastructure/di/injection.py
+++ src/infrastructure/di/injection.py
@@ -18,11 +18,15 @@ from application.use_cases.auth.login_user import LoginUserUseCase
 from application.use_cases.auth.register_user import RegisterUserUseCase
 from application.use_cases.auth.verify_token import VerifyTokenUseCase
 from application.use_cases.users_management.change_password import ChangePasswordUseCase
+from application.use_cases.users_management.password_recovery import PasswordRecoveryUseCase
+from application.use_cases.tickets.create_ticket import CreateTicketUseCase
+from application.use_cases.tickets.get_in_progress_ticket import GetInProgressTicketUseCase
 from infrastructure.database.database import get_db
 from infrastructure.repositories.sql_device_repository import SQLDeviceRepository
 from infrastructure.repositories.sql_provision_repository import SQLProvisionRepository
 from infrastructure.serializers.xmltodict_serializer import XmlToDictSerializer
 from infrastructure.auth.repositories.sql_user_repository import SQLUserRepository
+from infrastructure.auth.repositories.ticket_repository import SQLTicketRepository
 from infrastructure.auth.jwt_provider import JWTProvider
 from infrastructure.auth.password_hasher import PasswordHasher
 from config import settings
@@ -42,6 +46,10 @@ def get_provision_repository(db: AsyncSession = Depends(get_db)) -> ProvisionRep
 
 def get_user_repository(db: AsyncSession = Depends(get_db)) -> UserRepository:
     return SQLUserRepository(db_session=db)
+
+
+def get_ticket_repository(db: AsyncSession = Depends(get_db)) -> SQLTicketRepository:
+    return SQLTicketRepository(db_session=db)
 
 
 def get_xml_serializer() -> XmlSerializer:
@@ -178,6 +186,18 @@ def get_change_password_use_case(
     )
 
 
+def get_password_recovery_use_case(
+    ticket_repo: SQLTicketRepository = Depends(get_ticket_repository),
+    user_repo: UserRepository = Depends(get_user_repository),
+    password_hasher: PasswordHasher = Depends(get_password_hasher)
+) -> PasswordRecoveryUseCase:
+    return PasswordRecoveryUseCase(
+        ticket_repo=ticket_repo,
+        user_repo=user_repo,
+        password_hasher=password_hasher
+    )
+
+
 def get_verify_token_use_case(
     user_repo: UserRepository = Depends(get_user_repository),
     jwt_provider: JWTProvider = Depends(get_jwt_provider)
@@ -185,4 +205,22 @@ def get_verify_token_use_case(
     return VerifyTokenUseCase(
         user_repo=user_repo,
         jwt_provider=jwt_provider
+    )
+
+
+def get_create_ticket_use_case(
+    ticket_repo: SQLTicketRepository = Depends(get_ticket_repository),
+    user_repo: UserRepository = Depends(get_user_repository)
+) -> CreateTicketUseCase:
+    return CreateTicketUseCase(
+        ticket_repo=ticket_repo,
+        user_repo=user_repo
+    )
+
+
+def get_in_progress_ticket_use_case(
+    ticket_repo: SQLTicketRepository = Depends(get_ticket_repository)
+) -> GetInProgressTicketUseCase:
+    return GetInProgressTicketUseCase(
+        ticket_repo=ticket_repo
     )
